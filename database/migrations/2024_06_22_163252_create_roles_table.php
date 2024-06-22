d07f04b766b2590f17bba8ccf734204c7a51c765
Davor Spirelja <davor.spirelja@gmail.com>
2024-06-22 18:46:17 +0200

    create user and role migrations

---
+++ database/migrations/2024_06_22_163252_create_roles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+        });
+
+        DB::table('roles')->insert([
+            ['name' => 'admin'],
+            ['name' => 'journalist'],
+            ['name' => 'reader']
+        ]);
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
